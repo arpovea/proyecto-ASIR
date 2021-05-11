26c139fdadc21ad5c68dffd624a35181204ed390
Adri <arodriguezpovea@gmail.com>
2021-05-11 17:45:04 +0200

    Modificando terraform

--- networking.tf
+++ networking.tf
@@ -6,7 +6,7 @@ resource "google_compute_network" "vpc_proyecto_asir" {
 # Subnet
 resource "google_compute_subnetwork" "subnet_proyecto_asir" {
   name          = "subnet-proyecto-asir"
-  region        = var.region
+  region        = "europe-west1"
   network       = google_compute_network.vpc_proyecto_asir.name
   ip_cidr_range = "10.10.0.0/24"
 }
