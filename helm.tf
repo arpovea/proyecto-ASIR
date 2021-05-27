0076a369a3b0bd9694b10a3fc44ce676f39bcef9
Adrián Rodríguez <arodriguezpovea@gmail.com>
2021-05-27 21:55:17 +0200

    Cambios Terraform, agregando kubernetes provider, helm provider y configuracion de permisos

---
+++ helm.tf
@@ -0,0 +1,43 @@
+# Use this datasource to access the Terraform account's email for Kubernetes permissions.
+data "google_client_openid_userinfo" "terraform_user" {}
+
+
+resource "kubernetes_cluster_role_binding" "user" {
+  metadata {
+    name = "admin-user"
+  }
+
+  role_ref {
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+    api_group = "rbac.authorization.k8s.io"
+  }
+
+  subject {
+    kind      = "User"
+    name      = data.google_client_openid_userinfo.terraform_user.email
+    api_group = "rbac.authorization.k8s.io"
+  }
+
+  subject {
+    kind      = "Group"
+    name      = "system:masters"
+    api_group = "rbac.authorization.k8s.io"
+  }
+}
+
+# Creando namespace para argocd
+resource "kubernetes_namespace" "argocd" {
+    metadata {
+        name = "argocd"
+    }
+}
+
+
+# Desplegando argocd con helm
+resource "helm_release" "helm_argocd" {
+    name       = "argocd"
+    repository = "https://argoproj.github.io/argo-helm"
+    chart      = "argo-cd"
+    namespace = "argocd"
+}
